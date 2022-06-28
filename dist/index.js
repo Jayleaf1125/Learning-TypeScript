24aee87fc7ffc80f89f479abae2c86884db4dba9
Jayleaf1125 <jasonpadilla104@gmail.com>
2022-06-27 20:56:21 -0400

    Traversy Media Crash Course

---
+++ dist/index.js
@@ -0,0 +1,87 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Marcy Lab';
+let publish = false;
+let x = 'Hello';
+x = true;
+let age;
+age = 30;
+let ids = [1, 2, 3, 4, 5, 6];
+let arr = [1, 'h', true];
+// Tuple
+let person = [1, 'John', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'John'],
+    [2, 'May'],
+    [3, 'Max'],
+];
+// Union
+let pid;
+pid = '22';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'John'
+};
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid;
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: 'John'
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+// Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const brad = new Person(1, "Brad");
+const mike = new Person(2, "Mike");
+// Inhertiance & Subclasses
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.position = position;
+    }
+}
+const emp = new Employee(3, 'Jason', 'CEO');
+// Generics
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArray = getArray([1, 2, 3, 4]);
+let strArray = getArray(['brad', 'job', 'mike', 'kitye']);
+// strArray.push(1)
